cf1c8801a967c310889c644ba5a31a2fb7589414
M Rijal Al Fariz <rijalfariz17@gmail.com>
2021-03-08 13:25:43 +0700

    Add permission to feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -57,8 +57,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 class ProfileFeedItem(models.Model):
     """Profile status update"""
     user_profile = models.ForeignKey(
-        #or UserProfile
-        settings.AUTH_USER_MODEL,
+        settings.AUTH_USER_MODEL,  #or UserProfile
         on_delete=models.CASCADE,
     )
     status_text = models.CharField(max_length=255)
